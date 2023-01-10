d76e80317b44f47de113e495ac013372c62e8f44
tigersing <108626977+tigersing@users.noreply.github.com>
2023-01-10 21:39:33 +0200

    Update task6.py
    new

--- task6/task6.py
+++ task6/task6.py
@@ -5,50 +5,49 @@ END = 'files/end.log'
 
 
 def convert_to_dict(x):
-    file = with open(x, mode='r', encoding='utf-8') as file_2:
-    onstring = file.readlines()
+    with open(x, mode='r', encoding='utf-8') as file:
+        onstring = file.readlines()
     st = {}
     for i in onstring:
         k = i[0:3]
         v = i[3:]
         v = v.strip()
-        if not v.isalpha() :
-            v = datetime.strptime(v, "%Y-%m-%d_%H:%M:%S.%f")
-        else:
-            return v
-        st[k] = v # добавляем в словарь соответствующие пары ключ:значение
-
-# Convert files to dictionary
-abr = convert_to_dict(ABR)
-st = convert_to_dict(ST)
-end = convert_to_dict(END)
+        v = datetime.strptime(v, "%Y-%m-%d_%H:%M:%S.%f")
+        st[k] = v
+        return st
 
 
 # Timedelta from dict end and st
 def delta_dictionaries(x,y):
-    res = {key: end[key] - st.get(key, 0) for key in end.keys()}
+    res = {key: x[key] - y.get(key, 0) for key in x.keys()}
     return res
 
+
 # Sort dictionary values
-def sort_dictionary():
-    sorted_res = sorted(res.items(), key=lambda x:x[1],      reverse=True)
+def sort_dictionary(res):
+    sorted_res = sorted(res.items(), key=lambda x: x[1], reverse=True)
     sorted_res = dict(sorted_res)
     return sorted_res
 
-# Convert timedelta into string
-def sort_dictionary():
-    res = dict()
-    for sub in sorted_res:
-            res[sub] = str(sorted_res[sub])
-        return res
+
+if __name__ == "__main__":
+    # Convert files to dictionary
+    abr = convert_to_dict(ABR)
+    st = convert_to_dict(ST)
+    end = convert_to_dict(END)
+
+# Timedelta result
+res = delta_dictionaries(end, st)
 
 
+# Building report by adding 2 dictionaries using keys
+def build_report (x, y):
+    report = {key: x[key] + y.get(key, 0) for key in x.keys()}
 
-if name == "main":
-    def combine_dictionaries(x, y):
-        final = {}
-        final = {key: res[key] + abr.get(key, 0) for key in res.keys()}
-        return final
+final = build_report(res, abr)
 
-    def print_report():
-        print(final)
+
+def print_report ():
+    print(final)
+
+print_report()
