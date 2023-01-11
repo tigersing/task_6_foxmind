333147bfb0b8084a7bdd5df41c9b723d5eb0d46b
tigersing <108626977+tigersing@users.noreply.github.com>
2023-01-11 08:00:37 +0200

    Update task6.py
    reformat

--- task6/task6.py
+++ task6/task6.py
@@ -1,53 +1,45 @@
 from datetime import datetime
+
 ABR = 'files/abbreviations.txt'
 ST = 'files/start.log'
 END = 'files/end.log'
 
 
-def convert_to_dict(x):
+def convert_to_dict(x, time_parser=False):
     with open(x, mode='r', encoding='utf-8') as file:
-        onstring = file.readlines()
+        on_string = file.readlines()
     st = {}
-    for i in onstring:
+    for i in on_string:
         k = i[0:3]
         v = i[3:]
-        v = v.strip()
-        v = datetime.strptime(v, "%Y-%m-%d_%H:%M:%S.%f")
+        v = datetime.strptime(v.strip(), "%Y-%m-%d_%H:%M:%S.%f") if time_parser else v
         st[k] = v
-        return st
+    return st
 
 
 # Timedelta from dict end and st
-def delta_dictionaries(x,y):
-    res = {key: x[key] - y.get(key, 0) for key in x.keys()}
-    return res
+def delta_dictionaries(x, y):
+    return {key: (x[key] - y.get(key, 0)) for key in x.keys()}
 
 
 # Sort dictionary values
 def sort_dictionary(res):
-    sorted_res = sorted(res.items(), key=lambda x: x[1], reverse=True)
+    sorted_res = sorted(res.items(), key=lambda x: x[1].microseconds)
     sorted_res = dict(sorted_res)
     return sorted_res
 
 
-if __name__ == "__main__":
-    # Convert files to dictionary
+def print_report():
     abr = convert_to_dict(ABR)
-    st = convert_to_dict(ST)
-    end = convert_to_dict(END)
+    st = convert_to_dict(ST, True)
+    end = convert_to_dict(END, True)
 
-# Timedelta result
-res = delta_dictionaries(end, st)
-
-
-# Building report by adding 2 dictionaries using keys
-def build_report (x, y):
-    report = {key: x[key] + y.get(key, 0) for key in x.keys()}
-
-final = build_report(res, abr)
+    laps_data = sort_dictionary(delta_dictionaries(end, st))
+    for k in laps_data.keys():
+        _, name, team = abr[k].split('_')
+        lap_time = laps_data[k]
+        print(f"{name.strip()} | {team.strip()} | {str(lap_time)}")
 
 
-def print_report ():
-    print(final)
-
-print_report()
+if __name__ == "__main__":
+    print_report()
